48d6c0b5a04671f0a71a14ca1c69f405589fe47f
Joseph <josephfabrello@gmail.com>
2022-04-25 23:21:39 +1000

    Added redirects list of urls

---
+++ URL_Obfuscator/obfuscate.py
@@ -0,0 +1,22 @@
+# uses redirects to obfuscate urls
+# These urls give warnings to avoid improper usage
+# This code was created for educational purposes only
+# urls taken from Anish - M : https://github.com/Anish-M-code
+
+
+redirects = ['https://www.google.com/url?q=',
+             'https://google.com/url?q='    ,
+             'https://facebook.com/l.php?u='
+            ]
+
+def grab_url():
+    pass
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    pass
